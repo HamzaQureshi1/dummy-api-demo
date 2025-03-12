d6befff0972269bb3d5588f85eee7da72b3b6287
HamzaQureshi1 <hqureshi6594@gmail.com>
2025-03-12 21:50:36 +0000

    add perfomance to before-changes

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
+const { performance } = require('perf_hooks');
+
+app.use((req, res, next) => {
+  const start = performance.now();
+  
+  res.on('finish', () => {
+    const duration = performance.now() - start;
+    console.log(`🕒 ${req.method} ${req.originalUrl} - ${duration.toFixed(2)}ms`);
+  });
+
+  next();
+});
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
